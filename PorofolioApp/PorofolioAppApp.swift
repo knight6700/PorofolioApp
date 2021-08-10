f2f022cd66de571fed480cbab119239454099dcd
Mahmoud fares <mfnaguib@banquemisr.com>
2021-08-10 10:37:10 +0200

    Initial Commit

---
+++ PorofolioApp/PorofolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorofolioAppApp.swift
+//  PorofolioApp
+//
+//  Created by Mahmoud Fares on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PorofolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
